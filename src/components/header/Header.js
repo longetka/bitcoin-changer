0f96ed3682b2d8a41544020a347392e31ba4a186
longetka <longetka@gmail.com>
2022-07-19 00:15:12 +0700

    change logotype

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import {Wrapper, Picture, Navbar} from '../index';
-import logoPic from '../../assets/images/icons/Logotype.png';
+import {Wrapper, Picture, Navbar, Title} from '../index';
+import logoPic from '../../assets/images/icons/FElogo2.png';
 import navItems from './navItemsData';
 
 const Header = () => {
@@ -16,8 +16,7 @@ const Header = () => {
             <Picture 
                 src={logoPic} 
                 alt="logotype"
-                setWidth="183px"
-                setHeight="84px"
+                setWidth="25%"
             />
             <Navbar items={navItems} />
         </Wrapper>
