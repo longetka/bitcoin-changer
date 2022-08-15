b43c934f76263ae4b1894b01f27824e67a525533
longetka <longetka@gmail.com>
2022-08-15 22:09:21 +0700

    delete unused vars

--- src/components/navbar/Navbar.styles.js
+++ src/components/navbar/Navbar.styles.js
@@ -1,7 +1,6 @@
 import styled from "styled-components";
 import WrapperStyles from "../wrappers/Wrapper.styles";
 import MenuButtonStyled from "../menuButton/MenuButton.styles";
-import ListStyle from "../list/List.styles";
 
 const NavbarStyled = styled(WrapperStyles)`
   @media only screen and (min-width: 320px) and (max-width: 500px) {
